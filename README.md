71ad9b7818b90a4a537bc5d66cb243b335dd41b6
HowXu <howxu366@outlook.com>
2025-04-14 20:07:22 +0800

    [init] track initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Kotlin HttpServer
+
+Kotlin练手Demo 简单的Http服务器  
+推送Github存档
---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    kotlin("jvm") version "2.0.21"
+}
+
+group = "cn.howxu.kotlin_httpserver"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+}
+
+kotlin {
+    jvmToolchain(21)
+}
